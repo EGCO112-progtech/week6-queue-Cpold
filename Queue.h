c4e0f5511595faa3d880f157a3b7b9e8b4255b02
Cpold <chayapol.dec@student.mahidol.edu>
2025-02-12 08:41:28 +0000

    4 files

--- Queue.h
+++ Queue.h
@@ -9,7 +9,13 @@ typedef struct {
 void enqueue_struct(Queue* q, int x){
   Node *new_node=(Node*) malloc(sizeof(Node));
 if(new_node){ 
-  /*Finish enqueue */
+  new_node->data=x;
+  new_node->nextPtr=NULL;
+  if(q->size==0)
+  q->headPtr=new_node;
+  else q->tailPtr->nextPtr=new_node;
+  q->tailPtr=new_node;
+  q->size++;
  }
 }
 
@@ -18,10 +24,13 @@ int dequeue_struct(Queue *q){
    NodePtr t=q->headPtr;
    if(t){
    int value= t->data;
-       /*Finish dequeue */
+   q->headPtr=t->nextPtr;
+   if(q->size==1) q->tailPtr=NULL;
+   free(t);
+   q->size--;
    return value;
    }
-   printf("Empty queue");
+   printf("Empty queue\n");
    return 0;
 }
 
